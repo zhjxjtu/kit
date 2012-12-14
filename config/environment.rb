72e3ce664d19fa34e114404f5c26e98c1c5b75fd
Jing Zhang <zhjxjtu@gmail.com>
2012-12-14 06:46:08 -0800

    Revert "Email problem, to be tested"
    This reverts commit a524e8da23252f24756d2c875c16b1dd802516a0.

--- config/environment.rb
+++ config/environment.rb
@@ -8,12 +8,12 @@ Kit::Application.initialize!
 Kit::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :domain => "gmail.com",
-    :authentication => "plain",
-    :user_name => "focustest.2012@gmail.com",
-    :password => "Fifafifa99@focus",
-    :enable_starttls_auto => true
+  	:address => "smtp.gmail.com",
+  	:port => 587,
+  	:domain => "gmail.com",
+  	:authentication => "plain",
+  	:user_name => "focustest.2012@gmail.com",
+  	:password => "Fifafifa99@focus",
+  	:enable_starttls_auto => true
   }
 end
