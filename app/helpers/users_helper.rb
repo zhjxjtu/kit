96c76171b35f35170fe47778e77132ed570d4665
Jing Zhang <zhjxjtu@gmail.com>
2012-12-14 06:35:26 -0800

    Revert "passwords controller generated, update password ready"
    This reverts commit 27927e727ed06447fcbef368adab34dd18c129cd.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,9 @@
 module UsersHelper
   def update_without_password(user, params)
-  	user.update_attribute(:name, params[:name])&&user.update_attribute(:phone, params[:phone])&&user.update_attribute(:description, params[:description])
+  	if user.update_attribute(:name, params[:name])&&user.update_attribute(:phone, params[:phone])&&user.update_attribute(:description, params[:description])
+      return true
+    else
+      return false
+    end
   end
 end
