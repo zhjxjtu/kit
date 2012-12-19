d62abdd7f273eef5747bea3c1af59fd45c2f84bd
Jing Zhang <zhjxjtu@gmail.com>
2012-12-19 09:40:02 -0800

    layout updated 1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -72,6 +72,10 @@ module ApplicationHelper
 
   # Relationships related --------------------
 
+  def admin?
+    current_user.admin
+  end
+
   def create_relationship (invitation, user)
   	relationship = Relationship.new(invitation_id: invitation.id, inviter_id: invitation.user_id, invitee_id: user.id)
   	relationship.save
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+  validates :name, presence: true, length: { minimum: 2 }
 
   private
 
