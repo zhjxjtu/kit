7dd444587f57e65760eca63acca4d327832ee419
Jing Zhang <zhjxjtu@gmail.com>
2012-12-21 16:28:03 -0800

    layouts: icon added instead of text

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -56,21 +56,21 @@ class InvitationsController < ApplicationController
     @invitation.email = @invitation.email.downcase
   	if add_self?(@invitation.email)
       flash[:error] = "You can't add yourself"
-      redirect_to edit_invitation_path(current_user)
+      redirect_to contact_path(current_user)
     elsif current_contact_by_email?(@invitation.email)
       flash[:notice] = "You already have the information of " + User.find_by_email(@invitation.email).name + " (#{@invitation.email})"
-      redirect_to edit_invitation_path(current_user)
+      redirect_to contact_path(current_user)
     elsif already_invited_by?(@invitation.email)
       accept_original_invitation(@invitation)
       flash[:notice] = "You are now connected with" + User.find_by_email(@invitation.email).name + " (#{@invitation.email})"
-      redirect_to edit_invitation_path(current_user)
+      redirect_to contact_path(current_user)
     elsif existing_invitation_to?(@invitation.email)
       flash[:success] = "An reminding email sent to #{@invitation.email}"
-      redirect_to edit_invitation_path(current_user)
+      redirect_to invitation_path(current_user)
       send_invitation(@invitation)
     elsif @invitation.save
       flash[:success] = "An invitation sent to #{@invitation.email}"
-      redirect_to edit_invitation_path(current_user)
+      redirect_to invitation_path(current_user)
       send_invitation(@invitation)
   	else
   	  flash[:error] = @invitation.errors.full_messages[0]
